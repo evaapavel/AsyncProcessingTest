7044b6a4d5f1120273cc90932d88d15d26533097
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-09 09:53:36 +0200

    FIX Make the async version faster for the first time.

--- AsyncProcessingSol/AsyncProcessingMain/AsyncBreakfast/BreakfastProcessAsync.cs
+++ AsyncProcessingSol/AsyncProcessingMain/AsyncBreakfast/BreakfastProcessAsync.cs
@@ -107,7 +107,8 @@ namespace AsyncProcessingMain.AsyncBreakfast
                 Console.WriteLine("Putting a slice of bread in the toaster");
             }
             Console.WriteLine("Start toasting...");
-            Task.Delay(3000).Wait();
+            //Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Remove toast from the toaster");
 
             return new Toast();
@@ -130,13 +131,15 @@ namespace AsyncProcessingMain.AsyncBreakfast
         {
             Console.WriteLine($"putting {slices} slices of bacon in the pan");
             Console.WriteLine("cooking first side of bacon...");
-            Task.Delay(3000).Wait();
+            //Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             for (int slice = 0; slice < slices; slice++)
             {
                 Console.WriteLine("flipping a slice of bacon");
             }
             Console.WriteLine("cooking the second side of bacon...");
-            Task.Delay(3000).Wait();
+            //Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Put bacon on plate");
 
             return new Bacon();
@@ -147,10 +150,12 @@ namespace AsyncProcessingMain.AsyncBreakfast
         private static async Task<Egg> FryEggsAsync(int howMany)
         {
             Console.WriteLine("Warming the egg pan...");
-            Task.Delay(3000).Wait();
+            //Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine($"cracking {howMany} eggs");
             Console.WriteLine("cooking the eggs...");
-            Task.Delay(3000).Wait();
+            //Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Put eggs on plate");
 
             return new Egg();
